58afadfc11acf2c68731d175f5bc9adb46e1dba0
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-07-13 13:47:30 -0300

    ajuste de navbar para page de imc

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import './App.css' 
+import './App.css'
 
-// Páginas e Componentes
 import Footer from "./components/footer/Footer"
 import Navbar from "./components/navbar/Navbar"
 import Home from "./pages/home/Home"
-import ImcPage from './pages/Imc' // Import da branch imc
+import ImcPage from './pages/Imc'
 import ListarCategorias from "./pages/categorias/ListarCategorias"
 import FormCategoria from './components/categoria/formcategoria/FormCategoria'
 import DeletarCategoria from './components/categoria/deletarcategoria/DeletarCategoria'
@@ -21,16 +20,13 @@ function App() {
 
         <div className="min-h-[80vh]">
           <Routes>
-            {/* Rotas Principais e do IMC */}
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/imc" element={<ImcPage />} />
-
-            {/* Rotas de Categorias */}
             <Route path="/categorias" element={<ListarCategorias />} />
             <Route path="/cadastrarcategoria" element={<FormCategoria />} />
             <Route path="/editarcategoria/:id" element={<FormCategoria />} />
-            <Route path="/categorias/deletar/:id" element={<DeletarCategoria />} />   
+            <Route path="/categorias/deletar/:id" element={<DeletarCategoria />} />
           </Routes>
         </div>
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -11,6 +11,7 @@ function Navbar() {
                 <div className='flex gap-6 items-center text-gray-700'>
                     <Link to='/home' className='hover:text-green-700 transition'>Home</Link>
                     <Link to='/categorias' className='hover:text-green-700 transition'>Categorias</Link>
+                    <Link to='/imc' className='hover:text-green-700 transition'>Calcule seu IMC</Link>
                 </div>
             </div>
         </div>
